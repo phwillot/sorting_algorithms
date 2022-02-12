a70e9e04ac47510b51110a9e99c34dd27e2c267f
WILLOT Philippe <willot.ph@gmail.com>
2022-02-12 12:39:36 +0000

    Adding insertion_sort sorting algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,7 @@
 #include "sort.h"
 
+void swap_node(listint_t *current, listint_t *prev, listint_t **list);
+
 /**
  * insertion_sort_list - Sorts a doubly linked list
  * of integers in ascending order using the Insertion
@@ -20,5 +22,34 @@ void insertion_sort_list(listint_t **list)
 
 	while (current != NULL)
 	{
+		while (current->prev && current->n < current->prev->n)
+		{
+			swap_node(current, current->prev, list);
+			print_list(*list);
+		}
+		current = current->next;
 	}
+}
+
+/**
+ * swap_node - Swap two nodes of dlinkedlist
+ * @current: current node to swap
+ * @prev: previous node to swap with current
+ * @list: pointer of pointer of head dlinkedlist
+ *
+ * Return: Void
+ */
+
+void swap_node(listint_t *current, listint_t *prev, listint_t **list)
+{
+	if (prev->prev)
+		prev->prev->next = current;
+	else
+		*list = current;
+	if (current->next)
+		current->next->prev = prev;
+	prev->next = current->next;
+	current->next = prev;
+	current->prev = prev->prev;
+	prev->prev = current;
 }
